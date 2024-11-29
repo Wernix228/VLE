e0e30d2aaf09aa9f9a8a26d66ea73f2723e60399
Wernix <102924429+Wernix228@users.noreply.github.com>
2024-11-30 02:07:10 +0300

    fix Animation

--- Assets/Scripts/KardAnimation.cs
+++ Assets/Scripts/KardAnimation.cs
@@ -5,17 +5,7 @@ public class KardAnimation : MonoBehaviour
     [SerializeField]
     Animator animator;
     bool active = false;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void onPress()
     {
         active = !active;
